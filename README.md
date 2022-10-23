8884fbba0a62e84114a65ecc966bf892138dd129
Mohamed Ali <mo7amedaliebaid@gmail.com>
2022-10-23 22:15:23 +0200

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # run_tracker
+A flutter run tracker app.
 
-Youtube Demo Videos https://youtube.com/playlist?list=PLCWp9wi0RFPs_6dSxs-RYON8QkbOO3JRI&si=vWDJ8t5X8Y09z5cG
+### State Management
+- Riverpod
+
+## Demo Video
+<a href="https://youtube.com/playlist?list=PLCWp9wi0RFPs_6dSxs-RYON8QkbOO3JRI&si=vWDJ8t5X8Y09z5cG"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" width="170"></img></a>
+
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_macos
+import flutter_tts
+import geolocator_apple
+import path_provider_foundation
+import shared_preferences_foundation
+import wakelock_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+}
